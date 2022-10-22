ef795f41c356e8b0dd67a4c402e469b75fef5cd7
Donertv <115746957+Donertv@users.noreply.github.com>
2022-10-22 10:54:34 +0200

    My First Site

---
+++ ScrollReveal.js
@@ -0,0 +1,8 @@
+ScrollReveal({ 
+    reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay: 150
+});
+
+ScrollReveal().reveal('.Content, p, .top-content, .social-links, .photo-gallery, .footer-col, .Tittle, .service, .about__us, Heading', { delay: 250, origin: 'bottom' });
